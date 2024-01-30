42fe03b1d5bd47f7aa3984535ad95af083d75a95
Yasmine <yasmine.chaiboubi@outlook.fr>
2024-01-30 17:11:49 +0100

    annonce controller

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -11,13 +11,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[Route('/api/annonce')]
+#[Route('/api')]
 class AnnonceController extends AbstractController
 {
     public function __construct(private AnnonceRepository $repo, private EntityManagerInterface $em) {}
 
-    #[Route(methods:'GET')]
+    #[Route('/annonce',methods:'GET')]
     public function all(Request $request): JsonResponse
     {
         $page = $request->query->get('page', 1);
@@ -26,26 +27,41 @@ class AnnonceController extends AbstractController
         return $this->json($this->repo->findBy([], limit: $pageSize, offset: ($page-1)*$pageSize));
     }
 
-    #[Route('/{id}', methods: 'GET')]
+    #[Route('/annonce/{id}', methods: 'GET')]
     public function one(Annonce $annonce) {
         return $this->json($annonce);
     }
 
     #[Route('/annonce-create', methods: 'POST')]
-    public function add(Person $person, Request $request, SerializerInterface $serializer): JsonResponse {
+    // public function add(Person $person, Request $request, SerializerInterface $serializer): JsonResponse {
+    //     try {
+    //         $annonce = $serializer->deserialize($request->getContent(), Annonce::class, 'json');
+    //     } catch (\Exception $e) {
+    //         return $this->json('Invalid Body', 400);
+    //     }
+    //     $annonce->setIdPerson($person);
+    //     $this->em->persist($annonce);
+    //     $this->em->flush();
+    //     return $this->json($annonce, 201);
+
+    // }
+    public function add(Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
         try {
             $annonce = $serializer->deserialize($request->getContent(), Annonce::class, 'json');
-        } catch (\Exception $e) {
-            return $this->json('Invalid Body', 400);
+        } catch (\Exception $error) {
+            return $this->json('Invalid body', 400);
         }
-        $annonce->setIdPerson($person);
-        $this->em->persist($annonce);
-        $this->em->flush();
-        return $this->json($annonce, 201);
+        $errors = $validator->validate($annonce);
+        if ($errors->count() > 0) {
+            return $this->json(['errors' => $errors], 400);
+        }
+        $this->repo->persist($annonce);
 
+        return $this->json($annonce, 201);
     }
 
-    #[Route('/{id}', methods: 'DELETE')]
+    #[Route('annonce/{id}', methods: 'DELETE')]
     public function delete(Annonce $annonce): JsonResponse {
         $this->em->remove($annonce);
         $this->em->flush();
